e3ab337c96ed9d1706ad067ddd5acdca05c0cc5e
Theara <thearachann134@gmail.com>
2025-03-08 17:22:35 +0700

    completed delete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,11 @@ import org.example.model.ProductModel;
 import org.example.view.StoreView;
 import org.example.controller.Controller;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         StoreView view = new StoreView();
         ProductModel model = new ProductModel();
         System.out.println(model.getProducts());
